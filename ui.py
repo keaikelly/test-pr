f892c051819ad3844dc4006af977f90d67eb6d5e
keaikelly <keaikelly@naver.com>
2023-01-17 22:30:39 +0900

    Add lbl1 in ui.py to print version info

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
-# ch 7.7.2 ui.py
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox) # QLineEdit, QComboBox 추가
+# ch 8.1.3 ui.py
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox, QLabel) # QLabel 추가
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore #모듈추가
 
@@ -14,6 +14,7 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
+        self.lbl1 = QLabel('v2.3.0', self) # 버전 정보 표시를 위한 lbl1 위젯 생성
         self.btn1=QPushButton('Calc',self) # 버튼 이름 변경
         self.btn2=QPushButton('Clear',self)
         
@@ -30,6 +31,7 @@ class View(QWidget):
 
         hbox=QHBoxLayout()
         hbox.addStretch(1)
+        hbox.addWidget(self.lbl1) # 버전 정보 표시를 위한 lbl1 위젯 생성
         hbox.addWidget(self.btn1)
         hbox.addWidget(self.btn2)
         hbox_formular = QHBoxLayout() # 새로 정의한 위젯을 QHBoxLayout에 배치
